e46a9fdfd06970cb2981b93d449b8176bf742bc7
ziv <zivh@gilat.com>
2023-01-08 15:45:25 +0200

    remove lops

--- mydocker/zip_job.py
+++ mydocker/zip_job.py
@@ -4,32 +4,22 @@ import zipfile
 
 arr = ['a', 'b', 'c', 'd']
 
-
-
-# Create the text files
-for elem in arr:
-    with open(elem + '.txt', 'w') as f:
-        pass
-
-
-# Check that all the text files have been created
-for elem in arr:
-    if not os.path.exists(elem + '.txt'):
-        print("Error: File {}.txt does not exist".format(elem))
-        sys.exit(1)
-
 # Get the value of the VERSION environment variable
 version = os.environ.get('VERSION', '1.0.0')
 
-# Create the ZIP files
+# Create the text files & Check that all the text files have been created
 for elem in arr:
+    with open(elem + '.txt', 'w') as f:
+        pass
+    if not os.path.exists(elem + '.txt'):
+        print("Error: File {}.txt does not exist".format(elem))
+        sys.exit(1)
     with zipfile.ZipFile(elem + '-' + version + '.zip', 'w') as z:
        z.write(elem + '.txt')
-       
-# Check that all the text files have been created
-for elem in arr:
     if not os.path.exists(elem + '-' + version + '.zip'):
         print("Error: File {}.zip does not exist".format(elem))
-        sys.exit(2)
+        sys.exit(1)
+
+
 
 print("All ZIP files have been created successfully")
